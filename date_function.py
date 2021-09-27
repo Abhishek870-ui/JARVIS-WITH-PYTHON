c4f5ff0241604c184bfa0bb83d09868c94c5f885
Abhishek870-ui <81959808+Abhishek870-ui@users.noreply.github.com>
2021-09-27 09:28:24 +0530

    Add files via upload

---
+++ date_function.py
@@ -0,0 +1,39 @@
+#date function
+# this function help us to tell date
+
+#time function
+# time function is used to tell us time 
+
+import  pyttsx3
+import  datetime
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+newVoiceRate = 120
+engine.setProperty('rate', newVoiceRate)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+#speak("Hello Welcome to all of you for voice assistant jarvis project")
+
+def time():
+    Time = datetime.datetime.now().strftime("the current time is  %I:%M:%S")
+    speak(Time)
+
+#time()
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+
+    speak("the current date is")
+    speak(date)
+    speak(month)
+    speak(year)
+
+date()
